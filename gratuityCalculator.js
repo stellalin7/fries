c33230707d3e532952dbc6bc4e64666f262788f3
Primo <primo@Primos-MacBook-Air.local>
2019-04-24 10:10:26 -0500

    tip calculator

--- gratuityCalculator.js
+++ gratuityCalculator.js
@@ -8,9 +8,9 @@ function totalWithGrat(billAmt){
 
 let billAmount = Math.floor(Math.random()*100);
 // generates random number between 0 and 99
-console.log("Bill Amount: $"+billAmount);
+console.log("Bill Amount: $" + billAmount);
 
 var tip = (prompt("What % would you like to tip?"))/100;
-console.log("You chose to tip "+ tip*100 +"%")
+console.log("You chose to tip "+ tip*100 + "%")
 
-console.log("Your total including gratuity is $"+totalWithGrat(billAmount).toFixed(2));
+console.log("Your total including gratuity is $" + totalWithGrat(billAmount).toFixed(2));
