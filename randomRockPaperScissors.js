79a97a9fa72eba652f915b313a04bd9921f78e93
Primo <primo@Primos-MacBook-Air.local>
2019-04-24 16:06:58 -0500

    calling getHand() on each round (instead of as a property in the player object definitions) to get a new hand each round

--- randomRockPaperScissors.js
+++ randomRockPaperScissors.js
@@ -21,18 +21,10 @@ var player2 = {
 function playRound(p1,p2){
 	p1.hand = getHand();
 	p2.hand = getHand();
-	console.log("Player 1 picked " + p1.hand +".");
-	console.log("Player 2 picked " + p2.hand +".");
+	console.log(p1.name + " picked " + p1.hand +".");
+	console.log(p2.name + " picked " + p2.hand +".");
 
-	if (p1.hand == 'rock' && p2.hand == 'rock'){
-		console.log("Tie");
-		var winner = null;
-	}
-	if (p1.hand == 'paper' && p2.hand == 'paper'){
-		console.log("Tie");
-		var winner = null;
-	}
-	if (p1.hand == 'scissors' && p2.hand == 'scissors'){
+	if (p1.hand === p2.hand){
 		console.log("Tie");
 		var winner = null;
 	}
@@ -76,8 +68,8 @@ function playRound(p1,p2){
 function playGame(p1,p2,minWins){
 	while (p1.wins < minWins && p2.wins < minWins){
 		var lastWinner = playRound(p1,p2);
-		console.log("Player 1 has won "+ p1.wins + " games this round.");
-		console.log("Player 2 has won "+ p2.wins + " games this round.");
+		console.log(p1.name +" has won "+ p1.wins + " games this round.");
+		console.log(p1.name + " has won "+ p2.wins + " games this round.");
 	}
 	return lastWinner;
 }
