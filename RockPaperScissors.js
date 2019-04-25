a42065fc2944982f98a6671e5a09d3c5580201cf
Primo <primo@Primos-MacBook-Air.local>
2019-04-25 14:30:54 -0500

    play 'Rock, Paper', 'Scissors' against the computer'

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,71 +1,79 @@
-var valid = false;
-while (!valid){
-	var player = prompt("Please enter 'r' for rock, 'p' for paper, or 's' for scissors:")
-	valid = true;
-	switch (player){
-		case 'r': 
-			console.log("You picked rock.");
+// var valid = false;
+// while (!valid){
+// 	var player = prompt("Please enter 'r' for rock, 'p' for paper, or 's' for scissors:")
+// 	valid = true;
+// 	switch (player){
+// 		case 'r': 
+// 			console.log("You picked rock.");
+// 			break;
+// 		case 'p':
+// 			console.log("You picked paper.");
+// 			break;
+// 		case 's':
+// 			console.log("You picked scissors.");
+// 			break;
+// 		default:
+// 			valid = false;
+// 	}
+// }
+
+
+var hands = ['rock','paper','scissors'];
+
+function getHand() {
+	var i = parseInt(Math.random()*10) % 3;
+	// generates random number between 0 and 2
+	return hands[i];
+}
+
+function playRock() {
+	var computer = getHand();
+	switch (computer){
+		case 'rock':
+			document.getElementById("message").innerHTML = "Computer picked rock.  It's a tie.";
 			break;
-		case 'p':
-			console.log("You picked paper.");
+		case 'paper':
+			document.getElementById("message").innerHTML = "Computer picked paper.  You win.";
 			break;
-		case 's':
-			console.log("You picked scissors.");
+		case 'scissors':
+			document.getElementById("message").innerHTML = "Computer picked scissors.  You lose.";
 			break;
-		default:
-			valid = false;
+
 	}
 }
 
-var x = Math.floor(Math.random()*99) % 3;
-// generates random number between 0 and 2
-switch (x){
-	case 0: 
-		var computer = 'rock';
-		break;
-	case 1:
-		var computer = 'paper';
-		break;
-	case 2:
-		var computer = 'scissors';
-		break;
+function playPaper() {
+	var computer = getHand();
+	switch (computer){
+		case 'rock':
+			document.getElementById("message").innerHTML = "Computer picked rock.  You win.";
+			break;
+		case 'paper':
+			document.getElementById("message").innerHTML = "Computer picked paper.  It's a tie.";
+			break;
+		case 'scissors':
+			document.getElementById("message").innerHTML = "Computer picked scissors.  You lose.";
+			break;
+
+	}
 }
 
-console.log("Computer picked " + computer +".");
+function playScissors() {
+	var computer = getHand();
+	switch (computer){
+		case 'rock':
+			document.getElementById("message").innerHTML = "Computer picked rock.  You lose.";
+			break;
+		case 'paper':
+			document.getElementById("message").innerHTML = "Computer picked paper.  You win.";
+			break;
+		case 'scissors':
+			document.getElementById("message").innerHTML = "Computer picked scissors.  It's a tie.";
+			break;
 
-if (player == 'r' && computer == 'rock'){
-	console.log("Tie");
-}
-if (player == 'p' && computer == 'paper'){
-	console.log("Tie");
-}
-if (player == 's' && computer == 'scissors'){
-	console.log("Tie");
-}
-
-if (player == 'r' && computer == 'paper'){
-	console.log("Computer wins.");
-	console.log("Paper beats rock.");
-}
-if (player == 'p' && computer == 'rock'){
-	console.log("You win.");
-	console.log("Paper beats rock.");
+	}
 }
 
-if (player == 's' && computer == 'rock'){
-	console.log("Computer wins");
-	console.log("Rock beats scissors.");
-}
-if (player == 'r' && computer == 'scissors'){
-	console.log("You win.");
-	console.log("Rock beats scissors.");
-}
 
-if (player == 'p' && computer == 'scissors'){
-	console.log("Computer wins.");
-	console.log("Scissors beats paper.");
-}
-if (player == 's' && computer == 'paper'){
-	console.log("You win");
-	console.log("Scissors beats paper.");
-}
+
+
